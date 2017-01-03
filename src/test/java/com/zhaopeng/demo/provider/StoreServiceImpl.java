f7797422ea980671c9f5c8465181d34e3148ebaf
zhaopeng <zp6282154@163.com>
2017-01-03 23:40:12 +0800

    msic:

--- src/test/java/com/zhaopeng/demo/provider/StoreServiceImpl.java
+++ src/test/java/com/zhaopeng/demo/provider/StoreServiceImpl.java
@@ -2,6 +2,9 @@ package com.zhaopeng.demo.provider;
 
 import com.zhaopeng.demo.api.StoreService;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by zhaopeng on 2016/11/15.
  */
@@ -11,5 +14,17 @@ public class StoreServiceImpl implements StoreService {
         System.out.println("被调用了 ！");
         String result="provider is invoking";
         return result;
+    }
+
+    public static  void main(String args[]){
+
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(new Date());
+            cal.set(Calendar.HOUR_OF_DAY, 0);
+            cal.set(Calendar.MINUTE, 0);
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+           System.out.println(cal.getTime());
+
     }
 }
